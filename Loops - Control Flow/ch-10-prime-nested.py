6cc72408c5aa0c6eb7b88e73364d8381d2466132
yc3t <ycetaibi@gmail.com>
2024-08-10 17:21:07 +0200

    finished loops challenges

---
+++ Loops - Control Flow/ch-10-prime-nested.py
@@ -0,0 +1,10 @@
+# print prime numbers from 1 to 100
+
+for n in range(1,100+1):
+    count = 0
+    for i in range(1,n+1):
+        if((n % i) == 0):
+            count +=1
+
+    if count == 2:
+        print(n)
---
+++ Loops - Control Flow/ch-11-patterns.py
@@ -0,0 +1,12 @@
+#we dont need loops to make a pattern, because of the * operator
+for i in range (5,0,-1):
+    print('*' * i)
+
+
+""" for i in range(0,5):
+    for j in range(0,5):
+        if i<=j:
+            print('*', end ='')
+    print('')
+
+ """
